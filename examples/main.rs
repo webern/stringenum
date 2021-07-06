2cebd8ef746eca3cb60bec67bec9b34f55b1018f
Matt Briggs <matthew.james.briggs@gmail.com>
2021-07-06 10:11:03 -0700

    cleanup

--- examples/main.rs
+++ examples/main.rs
@@ -5,23 +5,32 @@ use stringenum::StringEnum;
 enum Foo {
     Bar,
     Baz,
+    /// We can rename the variant's string equivalent like this. Note that we do
+    /// not support automatic capitalization changes, you need to do renames for
+    /// snake case or any other preference.
     #[stringenum(rename = "Bones the Cat")]
     Cat,
 }
 
+// the as_str function is const
 const BAZ_STR_CONST: &str = Foo::Baz.as_str();
 const BONES_THE_CAT: &str = Foo::Cat.as_str();
 
 fn main() {
+    // we can parse enum variants from their string equivalents
     let bar = Foo::from_str("Bar").unwrap();
     assert_eq!(bar, Foo::Bar);
     let baz = Foo::from_str("Baz").unwrap();
     assert_eq!(baz, Foo::Baz);
+
+    // the lifetime of as_str is static
     let bar_str: &'static str = Foo::Bar.as_str();
     assert_eq!("Bar", bar_str);
     let baz_str: &'static str = Foo::Baz.as_str();
     assert_eq!("Baz", baz_str);
     assert_eq!("Baz", BAZ_STR_CONST);
+
+    // partial eq works
     assert_eq!(Foo::Baz, "Baz");
     assert_eq!("Bones the Cat", Foo::Cat);
     assert_eq!(BONES_THE_CAT.to_string(), Foo::Cat);
--- src/lib.rs
+++ src/lib.rs
@@ -4,63 +4,63 @@ use proc_macro2::Ident;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::{quote, ToTokens, TokenStreamExt};
 // use syn::visit_mut::VisitMut;
-use syn::{
-    parse_macro_input, AttrStyle, Attribute, Data, DataEnum, DeriveInput, Lit, MetaNameValue,
-};
+use syn::{parse_macro_input, AttrStyle, Attribute, Data, DeriveInput, Lit, MetaNameValue};
 
 #[proc_macro_derive(StringEnum, attributes(stringenum))]
 pub fn stringenum(input: TokenStream) -> TokenStream {
-    // Parse the input tokens into a syntax tree
+    // Parse the input tokens
     let i = input.clone();
     let derive_input = parse_macro_input!(i as DeriveInput);
-    let mut streenum = Streenum::new();
+    let strenum = Strenum::new(&derive_input);
 
-    // Parse and modify source
-    // let mut ast: ItemEnum = syn::parse(input).expect("only works on enums");
-    streenum.enum_name = Some(derive_input.ident.clone());
-    let enum_data = match &derive_input.data {
-        Data::Enum(en) => en,
-        _ => panic!("only enums are supported"),
-    };
-    streenum.find_renames(enum_data);
-    // panic!("{:?}", streenum);
-    // let mut ast2: TokenStream2 = ast.to_token_stream().into();
+    // write function implementations to a token stream
     let mut ast2 = TokenStream2::new();
-    streenum.append_impls(&mut ast2);
+    strenum.write_impls(&mut ast2);
     ast2.into_token_stream().into()
-    // input
-    // TokenStream::new()
 }
 
-// #[proc_macro_attribute]
-// pub fn dummy(_attr: TokenStream, item: TokenStream) -> TokenStream {
-//     item
-// }
-
-/// Does the visiting and logic
+/// Represents the information we need in order to build the implementation functions.
 #[derive(Debug, Default)]
-struct Streenum {
+struct Strenum {
     enum_name: Option<Ident>,
     variants: Vec<Ident>,
     names: Vec<String>,
 }
 
-impl Streenum {
-    fn new() -> Self {
-        Self::default()
+impl Strenum {
+    /// Parse the `DeriveInput` for the information we need.
+    fn new(input: &DeriveInput) -> Self {
+        let mut streenum = Self::default();
+
+        streenum.enum_name = Some(input.ident.clone());
+        let enum_data = match &input.data {
+            Data::Enum(en) => en,
+            _ => panic!("only enums are supported"),
+        };
+
+        // check attribute arguments for rename strings and populate name vectors
+        for variant in &enum_data.variants {
+            let name =
+                find_rename_value(&variant.attrs).unwrap_or_else(|| variant.ident.to_string());
+            streenum.variants.push(variant.ident.clone());
+            streenum.names.push(name);
+        }
+
+        streenum
     }
 
-    fn append_impls(&self, ast: &mut TokenStream2) {
-        // let mut tokens = ast.into_token_stream();
+    fn write_impls(&self, ast: &mut TokenStream2) {
         let enum_name = self.enum_name.as_ref().unwrap();
+
+        // we two iters for each vec because they will be moved later
         let variant_name = self.names.iter();
         let variant_ident = self.variants.iter();
-
         let variant_name2 = self.names.iter();
         let variant_ident2 = self.variants.iter();
-        // #(#variants),*
+
         let code = quote!(
             impl #enum_name {
+                /// Return a `static`, `const` representation of the variant name.
                 pub const fn as_str(&self) -> &'static str {
                     match &self {
                         #( #enum_name::#variant_ident => #variant_name, )*
@@ -152,19 +152,7 @@ impl Streenum {
     }
 }
 
-impl Streenum {
-    fn find_renames(&mut self, enum_data: &DataEnum) {
-        // Let the default implementation do its thing, recursively.
-        // visit_mut::visit_item_enum_mut(self, node);
-        for variant in &enum_data.variants {
-            let name =
-                find_rename_value(&variant.attrs).unwrap_or_else(|| variant.ident.to_string());
-            self.variants.push(variant.ident.clone());
-            self.names.push(name);
-        }
-    }
-}
-
+/// Check all of the attributes to find a `stringenum(rename = "...")` if it exists.
 fn find_rename_value(attrs: &[Attribute]) -> Option<String> {
     for a in attrs {
         let maybe_rename = get_rename_value(a);
@@ -175,6 +163,7 @@ fn find_rename_value(attrs: &[Attribute]) -> Option<String> {
     None
 }
 
+/// Check an attribute to see if it is a `stringenum(rename = "...")` construct.
 fn get_rename_value(a: &Attribute) -> Option<String> {
     if !matches!(a.style, AttrStyle::Outer)
         || a.path.segments.len() != 1
@@ -199,16 +188,3 @@ fn get_rename_value(a: &Attribute) -> Option<String> {
 
     Some(s)
 }
-
-// impl VisitMut for Streenum {
-//
-//
-//     fn visit_variant_mut(&mut self, node: &mut Variant) {
-//         let ident = node.ident.clone();
-//         self.renames.push(None);
-//         self.variants.push(ident);
-//
-//         // Let the default implementation do its thing, recursively.
-//         visit_mut::visit_variant_mut(self, node)
-//     }
-// }
